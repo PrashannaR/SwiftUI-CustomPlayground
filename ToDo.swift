c494edea17549f7982bc55a9529124013f02f8e8
PrashannaR <prashanna.rajbhandari1@gmail.com>
2024-10-15 11:27:13 +0530

    chore: todo updated

---
+++ ToDo.swift
@@ -0,0 +1,12 @@
+//
+//  ToDo.swift
+//  SwiftUI-CustomPlayground
+//
+//  Created by Prashanna Rajbhandari on 15/10/2024.
+//
+
+/*
+ TODO:
+ 1. swipe menu
+ 2. apple like Toast implementation
+ */
