e59a599ec809ee2235ed0cfd5dd4ab940724ef0e
PrashannaR <prashanna.rajbhandari1@gmail.com>
2024-10-16 01:39:53 +0530

    feat: slide in view impl

--- Utils/Extensions/View.swift
+++ Utils/Extensions/View.swift
@@ -50,3 +50,48 @@ extension View {
         )
     }
 }
+
+
+public extension View {
+    /// Adds a slide in view onto the container view
+    ///
+    /// Create a manager:
+    /// ```
+    /// class SlideInViewManager: ObservableObject {
+    ///     @Published var isActive: Bool = false
+    /// }
+    /// ```
+    /// Initialize it:
+    /// ```
+    /// @StateObject private var slideInViewManager = SlideInViewManager()
+    /// ```
+    /// Use it:
+    /// ```
+    /// NavigationView {
+    ///     HomeView()
+    /// }
+    /// .navigationViewStyle(.stack)
+    /// .slideInView(isActive: $slideInViewManager.isActive) {
+    ///     MenuView()
+    ///         .edgesIgnoringSafeArea(.bottom)
+    /// }
+    /// .environmentObject(slideInViewManager)
+    /// ```
+    /// Access the `state` of the slide in view (`active` or `inactive`) or toggle the `state` from the `SlideInViewMananger` Envirinment Object
+    /// ```
+    /// @EnvironmentObject var slideInViewManager: SlideInViewManager
+    /// ```
+    /// - Parameters:
+    ///   - isActive: binding to show/hide the slide in view
+    ///   - edge: the slide in edge; default is `leading`
+    ///   - paddingPercentage: the percentage of the slide in view's padding; default is `0.35`
+    ///   - options: options; default is `SlideInViewOptions` defaults
+    ///   - content: the slide in view content
+    ///   - container: the container the slide in view will be presented on top of
+    /// - Returns: A view that has a slide in view
+    func slideInView<Content: View>(isActive: Binding<Bool>, edge: Edge = .leading, paddingPercentage: CGFloat = 0.3, options: SlideInViewOptions = SlideInViewOptions(), content: @escaping () -> Content) -> some View {
+        SlideInView(isActive: isActive, edge: edge, paddingPercentage: paddingPercentage, options: options, content: content) {
+            self
+        }
+    }
+}
